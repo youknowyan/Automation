365f0e7cdce7f8c309932c196ceb0adf2434799f
youknowyan <34096054+youknowyan@users.noreply.github.com>
2021-06-12 14:11:17 +1000

    Add files via upload

---
+++ myparamiko.py
@@ -0,0 +1,51 @@
+import paramiko
+import time
+
+# To ssh connect a server
+def connect(server_IP, server_Port, username, password):
+    ssh_client = paramiko.SSHClient()
+    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())     # To disregard Missing Host Key warning
+    print(f'Now connecting to server {server_IP}')                       # Display which device is connecting
+    ssh_client.connect(hostname=server_IP, port=server_Port, username=username, password=password,
+                       look_for_keys=False, allow_agent=False)           # Connect
+    return ssh_client
+
+# Open Shell for input purpose
+def open_shell(ssh_client):
+    shell = ssh_client.invoke_shell()
+    return shell
+
+# Type in command in shell
+def send_command(shell, command, timout=1):
+    print(f'Now sending command to server: {command}')
+    shell.send(command + '\n')     # '\n' to execute command
+    time.sleep(timout)
+
+# Show what is displaying on shell
+def show(shell, n=5000):
+    output = shell.recv(n)
+    return output.decode()
+
+# Close ssh client
+def close(ssh_client):
+    if ssh_client.get_transport().is_active() == True:   # Check if ssh client is still live
+        print('Closing connection')
+        ssh_client.close()
+
+### below is for test purpose
+if __name__ == '__main__':
+    router_1 = {'server_IP': '10.1.1.10', 'server_PORT': '22', 'username':'u1', 'password':'123'}
+    New_client = connect(**router_1)
+    shell = open_shell(New_client)
+
+    send_command(shell, 'enable')
+    send_command(shell, '123') # this is the enable password
+    send_command(shell, 'term len 0')
+    send_command(shell, 'show version')
+    send_command(shell, 'show ip int brief')
+
+    output = show(shell)
+    print(output)
+
+
+
